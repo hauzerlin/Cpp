77bcfe99700f5c2063e72ce6c03eb4e45b2d66ae
hauzerlin <zaq60509@gmail.com>
2022-06-28 19:09:07 +0800

    first files

---
+++ test.cpp
@@ -0,0 +1,11 @@
+//It's a test file for testing system can compile c++ file or not.
+#include<iostream>
+#include<cstdlib>
+
+using namespace std;
+
+int main()
+{
+	cout<<"Hello World!\n";
+	return 0;
+}
